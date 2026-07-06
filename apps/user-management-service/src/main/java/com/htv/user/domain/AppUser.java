5ffab6288b6cd8cc68ea9f8e40e88c6566608963
hatrongvu13 <hatrongvu13@gmail.com>
2026-07-06 15:57:55 +0700

    add skeleton app

--- apps/user-management-service/src/main/java/com/htv/user/domain/AppUser.java
+++ apps/user-management-service/src/main/java/com/htv/user/domain/AppUser.java
@@ -1,4 +1,35 @@
 package com.htv.user.domain;
 
-public record AppUser() {
+import com.htv.security.HtvSecurityProperties;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String passwordHash;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> roles = new HashSet<>();
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<String> permissions = new HashSet<>();
+
+    private boolean mfaEnabled;
+
+    @Enumerated(EnumType.STRING)
+    public HtvSecurityProperties.MfaMethod preferredMfaEnabled;
 }
--- apps/user-management-service/src/main/java/com/htv/user/repo/AppUserRepository.java
+++ apps/user-management-service/src/main/java/com/htv/user/repo/AppUserRepository.java
@@ -1,4 +1,13 @@
 package com.htv.user.repo;
 
-public class AppUserRepository {
+import com.htv.user.domain.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser, String> {
+    Optional<AppUser> findByUsernameOrEmail(String username, String email);
+    boolean existsByUsernameOrEmail(String username, String email);
 }
